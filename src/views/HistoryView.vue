acaf8c8137be3189603de4f64fe1e579dd79c2d9
huan.li <huan.li@transsion.com>
2026-03-27 10:22:09 +0800

    feat: HistoryView 完成，添加 GitHub Pages 自动部署

--- src/views/HistoryView.vue
+++ src/views/HistoryView.vue
@@ -1,5 +1,38 @@
 <template>
-  <div class="view">
-    <p>历史页（开发中）</p>
+  <div class="view history-view">
+    <h1 class="view__title">历史</h1>
+
+    <div v-if="index.length === 0" class="history-empty">
+      还没有日记，去今日页记录第一篇吧。
+    </div>
+
+    <div v-for="date in index" :key="date" class="history-item">
+      <button class="history-item__header" @click="toggle(date)">
+        <span class="history-item__date">{{ formatDate(date) }}</span>
+        <span class="history-item__arrow">{{ expanded[date] ? '▲' : '▼' }}</span>
+      </button>
+      <div v-if="expanded[date]" class="history-item__body">
+        <pre class="history-item__content">{{ getDiary(date)?.formatted }}</pre>
+      </div>
+    </div>
   </div>
 </template>
+
+<script setup>
+import { ref, onActivated } from 'vue'
+import { getDiaryIndex, getDiary } from '../utils/storage.js'
+
+const index = ref(getDiaryIndex())
+const expanded = ref({})
+
+onActivated(() => { index.value = getDiaryIndex() })
+
+function toggle(date) {
+  expanded.value[date] = !expanded.value[date]
+}
+
+function formatDate(dateStr) {
+  const d = new Date(dateStr)
+  return `${d.getFullYear()}年${d.getMonth() + 1}月${d.getDate()}日`
+}
+</script>
